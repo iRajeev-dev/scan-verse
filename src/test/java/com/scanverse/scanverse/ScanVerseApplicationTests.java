623a460f3a764ff6ae82a3caebc792f971aa8371
Rajeev <RajeevKumar.s@techsophy.com>
2024-06-08 20:19:44 +0530

    Creation of project

---
+++ src/test/java/com/scanverse/scanverse/ScanVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanverse.scanverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
